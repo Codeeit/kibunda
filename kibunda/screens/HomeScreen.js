2714a235757ffc1f37e07d95b748fcf3f4187292
Raymond B. Luanda <raymondbenedict4@gmail.com>
2023-02-09 16:00:20 +0000

    added screens folder

---
+++ kibunda/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+    View,
+    TouchableOpacity,
+    Text
+} from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
